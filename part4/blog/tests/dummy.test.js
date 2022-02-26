258f71854664c7f524eb02edfb0e992275f4c6ca
Andreas Levander <andreas.levander92@gmail.com>
2022-02-26 18:42:20 +0200

    done with 4.3 - 4.7

---
+++ part4/blog/tests/dummy.test.js
@@ -0,0 +1,9 @@
+import { dummy } from "../utils/list_helper";
+
+
+test('dummy returns one', () => {
+    const blogs = [];
+  
+    const result = dummy(blogs);
+    expect(result).toBe(1);
+})
--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -1,5 +1,66 @@
+import lodash from 'lodash';
+
 const dummy = (blogs) => {
     return 1;
-  }
+}
+
+const totalLikes = (blogs) => {
+    return blogs.length === 0
+    ? 0
+    : blogs.reduce((prev, {likes}) => (prev + likes), 0);
+}
+
+const favouriteBlog = (blogs) => {
+    let toplikes = -1;
+    let topblog = {};
+
+    blogs.forEach((blog) => {
+        if (blog.likes > toplikes) {
+            topblog = blog;
+            toplikes = blog.likes;
+        }
+    })
+
+    return {
+        title: topblog.title,
+        author: topblog.author,
+        likes: topblog.likes
+    }
+}
+
+const mostBlogs = (blogs) => {
+    const result = lodash.groupBy(blogs, 'author');
+
+    const mostblogs = { author: null, blogs: 0 };
+
+    for (const [key, value] of Object.entries(result)) {
+        if(value.length > mostblogs.blogs) {
+            mostblogs.author = key;
+            mostblogs.blogs = value.length;
+        }
+    }
+    return mostblogs;
+}
+
+const mostLikes = (blogs) => {
+    const result = {};
+
+    blogs.forEach((blog) => {
+        let name = blog.author;
+        result[name] = (isNaN(result[name]) ? 0 : result[name]) + blog.likes;
+    })
+    const mostlikes = {author: null,likes: -1};
+
+    for (const [author, likes] of Object.entries(result)) {
+        if(likes > mostlikes.likes) {
+            mostlikes.author = author;
+            mostlikes.likes = likes;
+        }
+    }
+    
+    return mostlikes;
+}
+
+
   
-export { dummy }
+export { dummy, totalLikes, favouriteBlog, mostBlogs, mostLikes }

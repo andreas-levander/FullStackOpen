966d6ee15efa261c76abfd430c7e261d64727dd4
Andreas Levander <andreas.levander92@gmail.com>
2022-05-29 17:05:47 +0300

    done with 6.13 and 6.14

--- part6/redux-anecdotes/src/components/AnecdoteForm.js
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,15 +1,18 @@
 import { useDispatch } from 'react-redux'
 import { newAnecdote } from '../reducers/anecdoteReducer'
 import { showNotif, hideNotif } from '../reducers/notifReducer'
+import * as anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = () => {
     const dispatch = useDispatch()
 
-    const addAnecdote = (event) => {
+    const addAnecdote = async (event) => {
         event.preventDefault()
         const content = event.target.anecdote.value
         event.target.anecdote.value = ''
-        dispatch(newAnecdote(content))
+
+        const returnedAnecdote = await anecdoteService.createNew(content)
+        dispatch(newAnecdote(returnedAnecdote))
 
         dispatch(showNotif(`added new anecdote '${content}'`))
         setTimeout(() => dispatch(hideNotif()), 5000)
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,53 +1,20 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
+import { createSlice } from "@reduxjs/toolkit"
 
-const getId = () => (100000 * Math.random()).toFixed(0)
-
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
+const anecdoteSlice = createSlice({
+  name: 'anecdotes',
+  initialState: [],
+  reducers: {
+    addVote: (state, action) => state.map(anecdote => 
+      anecdote.id === action.payload 
+      ? { ...anecdote, votes: anecdote.votes + 1 } 
+      : anecdote
+      ).sort((a, b) => b.votes - a.votes),
+    newAnecdote(state, action) {
+      state.push(action.payload)
+    },
+    setAnecdotes: (state, action) => action.payload
   }
-}
 
-export const addVote = (id) => {
-  return {
-    type: 'VOTE',
-    data: { id }
-  }
-}
-
-export const newAnecdote = (anecdote) => {
-  return {
-    type: 'NEW',
-    data: { anecdote }
-  }
-}
-
-const initialState = anecdotesAtStart.map(asObject)
-
-const reducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
-
-  switch (action.type) {
-    case 'VOTE':
-      return state.map(anecdote => 
-        anecdote.id === action.data.id 
-        ? { ...anecdote, votes: anecdote.votes + 1 } 
-        : anecdote
-        ).sort((a, b) => b.votes - a.votes)
-    case 'NEW':
-      return [ ...state, asObject(action.data.anecdote)]
-    default: return state
-  }
-}
-
-export default reducer
+})
+export const { addVote, newAnecdote, setAnecdotes } = anecdoteSlice.actions
+export default anecdoteSlice.reducer

f947b85ca8c34deb85d301177bd316c288935cc1
Andreas Fredrik Levander <andreas.levander@abo.fi>
2022-07-25 10:56:37 +0300

    done with 9.19 - 9.22

--- part9/patientor-frontend/src/PatientPage/index.tsx
+++ part9/patientor-frontend/src/PatientPage/index.tsx
@@ -3,8 +3,22 @@ import { setPatient, useStateValue } from "../state";
 import { useEffect } from "react";
 import axios from "axios";
 import { apiBaseUrl } from "../constants";
-import { Patient } from "../types";
-import { QuestionMark, Male, Female } from "@mui/icons-material";
+import {
+  Entry,
+  HealthCheckEntry,
+  HospitalEntry,
+  OccupationalHealthcareEntry,
+  Patient,
+} from "../types";
+import {
+  QuestionMark,
+  Male,
+  Female,
+  LocalHospital,
+  LocalPharmacy,
+  Work,
+} from "@mui/icons-material";
+import { Favorite } from "@material-ui/icons";
 
 const PatientPage = () => {
   const { id } = useParams<{ id: string }>();
@@ -38,6 +52,25 @@ const PatientPage = () => {
     }
   };
 
+  const assertNever = (value: never): never => {
+    throw new Error(
+      `Unhandled discriminated union member: ${JSON.stringify(value)}`
+    );
+  };
+
+  const EntryDetails = (entry: Entry) => {
+    switch (entry.type) {
+      case "Hospital":
+        return <HospitalEntryComponent entry={entry} />;
+      case "HealthCheck":
+        return <HealthCheckEntryComponent entry={entry} />;
+      case "OccupationalHealthcare":
+        return <OccupationalEntryComponent entry={entry} />;
+      default:
+        return assertNever(entry);
+    }
+  };
+
   return (
     <div>
       <h1>
@@ -47,6 +80,92 @@ const PatientPage = () => {
       </h1>
       <p>ssn: {patient.ssn}</p>
       <p>occupation: {patient.occupation}</p>
+      <h3>
+        <b>entries</b>
+      </h3>
+      {patient.entries?.map((entry) => (
+        <div key={entry.id} style={{ padding: "2px" }}>
+          {EntryDetails(entry)}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const entryStyle = {
+  border: "2px solid",
+  padding: "5px",
+  borderRadius: "10px",
+};
+
+const HospitalEntryComponent = ({ entry }: { entry: HospitalEntry }) => {
+  const [{ diagnoses }] = useStateValue();
+  return (
+    <div style={entryStyle}>
+      <p>
+        {entry.date} <LocalHospital />
+      </p>
+      <p>{entry.description}</p>
+      <ul>
+        {entry.diagnosisCodes?.map((code) => (
+          <li key={code}>
+            {code} {diagnoses.filter((d) => d.code === code)[0]?.name}
+          </li>
+        ))}
+      </ul>
+      <p>diagnose by {entry.specialist}</p>
+    </div>
+  );
+};
+
+const OccupationalEntryComponent = ({
+  entry,
+}: {
+  entry: OccupationalHealthcareEntry;
+}) => {
+  const [{ diagnoses }] = useStateValue();
+  return (
+    <div style={entryStyle}>
+      <p>
+        {entry.date} <Work />
+      </p>
+      <p>{entry.description}</p>
+      <ul>
+        {entry.diagnosisCodes?.map((code) => (
+          <li key={code}>
+            {code} {diagnoses.filter((d) => d.code === code)[0]?.name}
+          </li>
+        ))}
+      </ul>
+      <p>diagnose by {entry.specialist}</p>
+    </div>
+  );
+};
+
+const HealthCheckEntryComponent = ({ entry }: { entry: HealthCheckEntry }) => {
+  const [{ diagnoses }] = useStateValue();
+  return (
+    <div style={entryStyle}>
+      <p>
+        {entry.date} <LocalPharmacy />
+      </p>
+      <p>{entry.description}</p>
+      {entry.healthCheckRating === 0 && <Favorite style={{ color: "green" }} />}
+      {entry.healthCheckRating === 1 && (
+        <Favorite style={{ color: "yellowgreen" }} />
+      )}
+      {entry.healthCheckRating === 2 && (
+        <Favorite style={{ color: "yellow" }} />
+      )}
+      {entry.healthCheckRating === 3 && <Favorite style={{ color: "red" }} />}
+      <ul>
+        {entry.diagnosisCodes?.map((code) => (
+          <li key={code}>
+            {code} {diagnoses.filter((d) => d.code === code)[0]?.name}
+          </li>
+        ))}
+      </ul>
+      <p>diagnose by {entry.specialist}</p>
     </div>
   );
 };

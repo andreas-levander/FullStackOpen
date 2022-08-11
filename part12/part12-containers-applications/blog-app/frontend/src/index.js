4465d8cbc70a8664188a4ac1d5275f1d2ef84864
Andreas Levander <andreas.levander@abo.fi>
2022-08-11 09:54:11 +0300

    minor bugfix for 12.21

--- part12/part12-containers-applications/blog-app/frontend/src/index.js
+++ part12/part12-containers-applications/blog-app/frontend/src/index.js
@@ -3,8 +3,4 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 
-export const backend = process.env.BACKEND_URL;
-
-console.log(backend);
-
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
